369b63a902768349016e42c2271c69d1042865a1
comp-master-byte <demoniac781@gmail.com>
2023-03-24 01:25:45 +0300

    Меню навигации

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Navigation from "./components/Navigation/Navigation";
 import "./styles/global.scss";
 import MyButton from "./UI/MyButton/MyButton";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+import styles from "./Navigation.module.scss";
+import logo from "../../assets/logo.svg";
+import vk from "../../assets/vk.svg";
+
+import MyButton from "../../UI/MyButton/MyButton";
+
+const Navigation: FC = () => {
+  return (
+    <header className={styles.navigationWrapper}>
+      <div className={styles.container}>
+        <div className={styles.navigationInner}>
+          <div className={styles.navigationInner__logo}>
+            <img src={logo} alt="logo" />
+          </div>
+          <div className={styles.navigationLinks}>
+            <div className={styles.navigationLinks__link}>О нас</div>
+            <div className={styles.navigationLinks__link}>Как это работает</div>
+            <div className={styles.navigationLinks__link}>Вопросы и ответы</div>
+            <div className={styles.navigationLinks__link}>Контакты</div>
+          </div>
+          <div className={styles.navigationEntrance}>
+            <MyButton color="primary" variant="outlined" size="small">
+              Войти
+            </MyButton>
+            <img src={vk} alt="lk" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navigation;
