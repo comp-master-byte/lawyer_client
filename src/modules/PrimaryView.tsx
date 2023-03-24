a1894cdc7447531dd4cd512c89a01858170e1bfa
comp-master-byte <demoniac781@gmail.com>
2023-03-24 20:08:52 +0300

    Раположение иллустраций

--- src/modules/PrimaryView.tsx
+++ src/modules/PrimaryView.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import MyButton from "../UI/MyButton/MyButton";
 import styles from "./PrimaryView.module.scss";
-
+import pointer from "./public/pointer.svg";
+import human from "./public/yura.png";
 const PrimaryView = () => {
   return (
     <div className={styles.primaryViewWrapper}>
@@ -11,15 +12,30 @@ const PrimaryView = () => {
             <div className={styles.viewLeftColumn__title}>
               Решите свой <br /> юридический <br /> вопрос
             </div>
+            <div className={styles.leftColumnIllustration}>
+              <img
+                className={styles.leftColumnIllustration__img}
+                src={pointer}
+                alt="pointer"
+              />
+              <div className={styles.leftColumnIllustration__text}>
+                Задайте <br /> свой вопрос <br /> Юре
+              </div>
+            </div>
             <div className={styles.viewLeftColumn__btn}>
               <MyButton color="primary" size="large" variant="contained">
                 Задать вопрос
               </MyButton>
             </div>
           </div>
+
           <div className={styles.viewRightColumn}>
             <div className={styles.viewRightColumn__title}>
               Станьте юристом <br /> на нашей <br /> платформе
+            </div>
+            <div className={styles.rightColumnItalic}>
+              Если ты юрист и хочешь решать индивидуальные запросы наших
+              клиентов, то регистрируйся!
             </div>
             <div className={styles.viewRightColumn__btn}>
               <MyButton color="secondary" size="large" variant="contained">
@@ -27,6 +43,11 @@ const PrimaryView = () => {
               </MyButton>
             </div>
           </div>
+          <img
+            src={human}
+            className={styles.primaryViewInner__human}
+            alt="human"
+          />
         </div>
       </div>
     </div>
