2d5155817eb0417841d6bb99b04946a3177bbd43
comp-master-byte <demoniac781@gmail.com>
2023-04-02 16:44:50 +0300

    Карточка проблемы

--- src/widgets/home/aboutUs/components/solutionCard/SolutionCard.tsx
+++ src/widgets/home/aboutUs/components/solutionCard/SolutionCard.tsx
@@ -1,7 +1,20 @@
 import styles from "./SolutionCard.module.scss";
+import icon from "./assets/icon.svg";
 
 const SolutionCard = () => {
-  return <div className={styles.solutionCardWrapper}>SolutionCard</div>;
+  return (
+    <div className={styles.solutionCardWrapper}>
+      <div className={styles.solutionCardTitle}>
+        <div className={styles.solutionCardTitle__label}>Проблема</div>
+        <img src={icon} alt="icon" />
+      </div>
+      <p className={styles.solutionCardWrapper__text}>
+        На рынке оказания юридических услуг сложилось определенное недоверие к
+        юристам, так как они объясняют все сложным языком, и пытаются оказать
+        как можно больше консультаций за максимальную стоимость.
+      </p>
+    </div>
+  );
 };
 
 export default SolutionCard;
