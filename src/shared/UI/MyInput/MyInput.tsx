7152bfcdcd53b823f3b6904d44910fdd9d2559d5
comp-master-byte <demoniac781@gmail.com>
2023-04-23 03:23:32 +0300

    Инпут

---
+++ src/shared/UI/MyInput/MyInput.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from "react";
+import styles from "./MyInput.module.scss";
+
+interface MyInputProps {
+  error: string;
+  placeholder: string;
+  register: () => void;
+}
+
+const MyInput: FC<MyInputProps> = ({ register, error, placeholder }) => {
+  return (
+    <div className={styles.myInputWrapper}>
+      <input
+        className={styles.myInput}
+        {...register}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default MyInput;
