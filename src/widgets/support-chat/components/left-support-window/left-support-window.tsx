6ee735c7d6c39231cf17cbd9878dc642cd8e672b
comp-master-byte <demoniac781@gmail.com>
2023-08-21 17:56:48 +0300

    Верстка левого окна чата

--- src/widgets/support-chat/components/left-support-window/left-support-window.tsx
+++ src/widgets/support-chat/components/left-support-window/left-support-window.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import styles from "./left-support-window.module.scss";
-import classNames from 'classnames';
+import backArrowSvg from "../../assets/backArrow.svg";
+import logoSvg from "../../assets/logo.svg";
 
 const LeftSupportWindow: React.FC = () => {
     return (
-        <div className={classNames(styles.chatContentWrapper, styles.leftSupportChatWindow)}></div>
+        <section className={styles.leftSupportChatWindow}>
+            <header className={styles.leftWindowHeader}>
+                <button className={styles.leftWindowHeader__startBegin}>
+                    Начать сначала
+                </button>
+                <div className={styles.backToPrevQuestion}>
+                    <img src={backArrowSvg} alt='' />
+                    <p className={styles.backToPrevQuestion__text}>
+                        вернуться <br /> к предыдущему <br /> вопросу
+                    </p>
+                </div>
+            </header>
+
+            <div className={styles.mainLeftWindowContent}>
+                <img src={logoSvg} alt='' />
+                <h3 className={styles.contentMainTitle}>
+                    Приветствую! <br />
+                    Меня зовут Юра. <br /> 
+                    Я твой  юридический помощник.
+                </h3>
+                <p className={styles.contentHelperText}>
+                    Постараюсь помочь тебе разобраться в проблеме из области права. Выбери юридическую область, к которой относится твой вопрос.
+                </p>
+            </div>
+        </section>
     )
 }
 
