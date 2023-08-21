d062ad3501250bdf60afa60c8fd1d08923787b32
comp-master-byte <demoniac781@gmail.com>
2023-08-21 18:26:15 +0300

    Подключение redux toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./app/styles/global.scss";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app";
+import { setupStore } from "app/providers/store";
+import { Provider } from "react-redux";
+
+const store = setupStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,6 +14,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <BrowserRouter>
-        <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
     </BrowserRouter>
 );
