37a1cb93acd93cef556372a7bd289b9a21a38d99
comp-master-byte <demoniac781@gmail.com>
2023-08-21 19:13:49 +0300

    Кнопка - Начать сначала

--- src/widgets/support-chat/components/left-support-window/components/left-window-header/left-window-header.tsx
+++ src/widgets/support-chat/components/left-support-window/components/left-window-header/left-window-header.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from "./left-window-header.module.scss";
 import backArrowSvg from "../../../../assets/backArrow.svg";
+import { useAppDispatch } from 'shared/lib/hooks/redux';
+import { fetchMessageNode } from 'widgets/support-chat/model/async-actions';
 
 const LeftWindowHeader: React.FC = () => {
+    const dispatch = useAppDispatch();
+    
     return (
         <header className={styles.leftWindowHeader}>
-            <button className={styles.leftWindowHeader__startBegin}>
+            <button 
+                className={styles.leftWindowHeader__startBegin}
+                onClick={() => dispatch(fetchMessageNode(1))}
+            >
                 Начать сначала
             </button>
             <div className={styles.backToPrevQuestion}>
