ec0546e1ba6c423019ce484f44daa16c62117921
comp-master-byte <demoniac781@gmail.com>
2023-09-01 17:08:43 +0300

    Ссылки в чекбокс

--- src/shared/ui/checkbox/checkbox.tsx
+++ src/shared/ui/checkbox/checkbox.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from "./checkbox.module.scss";
+import { Link } from 'react-router-dom';
 
 interface CheckboxProps {
     // label: string;
@@ -11,7 +12,8 @@ const Checkbox: React.FC<CheckboxProps> = () => {
             <input className={styles.checkbox} type="checkbox" />
             <span className={styles.fake}></span>
             <span className={styles.text}>
-                Принимаю условия <span className={styles.colored}>Политики конфиденциальности</span> и <span className={styles.colored}>Пользовательского соглашения</span> 
+                Принимаю условия <Link to='/privacy-policy' className={styles.colored}>Политики конфиденциальности</Link> и 
+                <Link to='/terms-of-use' className={styles.colored}> Пользовательского соглашения</Link> 
             </span>
         </label>
     )
