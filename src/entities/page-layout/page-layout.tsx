c88d5d72be04a9cf409c3a376c3dd120cc71469a
comp-master-byte <demoniac781@gmail.com>
2023-09-03 12:58:27 +0300

    Helmet

--- src/entities/page-layout/page-layout.tsx
+++ src/entities/page-layout/page-layout.tsx
@@ -1,10 +1,11 @@
-import { fetchUser } from 'app/store/async-actions'
-import { userSlice } from 'app/store/userSlice'
-import React, { useEffect } from 'react'
-import { Outlet } from 'react-router-dom'
-import { useAppDispatch } from 'shared/lib/hooks/redux'
-import Footer from 'widgets/footer/footer'
-import Navigation from 'widgets/navigation/navigation'
+import React, { useEffect } from 'react';
+import { fetchUser } from 'app/store/async-actions';
+import { userSlice } from 'app/store/userSlice';
+import { Outlet } from 'react-router-dom';
+import { useAppDispatch } from 'shared/lib/hooks/redux';
+import Footer from 'widgets/footer/footer';
+import Navigation from 'widgets/navigation/navigation';
+import { Helmet } from 'react-helmet';
 
 const PageLayout: React.FC = () => {
     const dispatch = useAppDispatch();
@@ -23,6 +24,9 @@ const PageLayout: React.FC = () => {
     
     return (
         <div>
+            <Helmet>
+                <title>Юра прав - твой цифровой юридический помощник </title>
+            </Helmet>
             <Navigation />  
             <Outlet />
             <Footer />
