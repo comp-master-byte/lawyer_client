b002c380414c9eb68f8655d5c435e48003a2a98d
comp-master-byte <demoniac781@gmail.com>
2023-09-04 17:54:07 +0300

    Верстка ожидания ответа

--- src/pages/cabinet/waiting-lawyer-appeal/waiting-lawyer-appeal.tsx
+++ src/pages/cabinet/waiting-lawyer-appeal/waiting-lawyer-appeal.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import styles from "./waiting-lawyer-appeal.module.scss";
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import MyButton from 'shared/ui/MyButton/MyButton';
 
 const WaitingLawyerAppeal: React.FC = () => {
+    const navigate = useNavigate();
     const {id} = useParams();
 
     return (
-        <div>WaitingLawyerAppeal: React.FC</div>
+        <div className={styles.lawyerAppealWrapper}>
+            <header className={styles.lawyerAppealHeader}>
+                <MyButton
+                    color='primary'
+                    variant='outlined'
+                    size='large'
+                    btnClassName={styles.goBackButton}
+                    onClick={() => navigate(-1)}
+                >
+                    Вернуться назад
+                </MyButton>
+
+                <div className={styles.lawyerAppealTitle}>
+                    <h1 className={styles.lawyerAppealTitle__h1}>Обращение {id}</h1>
+                    <h2 className={styles.lawyerAppealTitle__h2}>Ожидает выбора юриста</h2>
+                </div>
+            </header>
+        </div>
     )
 }
 
