c63ab982f1e1600f2d88f04df8d65dacde1bb780
comp-master-byte <demoniac781@gmail.com>
2023-09-05 12:19:21 +0300

    Поправил верстку главного экрана

--- src/widgets/navigation/navigation.tsx
+++ src/widgets/navigation/navigation.tsx
@@ -70,102 +70,69 @@ const Navigation: React.FC = React.memo(function Navigation() {
             <SuccessRegisterModal />
           
             <div className={styles.container}>
-                <div className={styles.navigationInner}>
-                    <Link to='/'>
-                      <div className={styles.navigationInner__logo}>
-                          <img src={logo} alt="logo" />
-                      </div>
-                    </Link>
-                    <div className={classNames(styles.navigationLinks, {
-                      [styles.activeMobileNavigation]: isNavigationMobileVisible,
-                      [styles.closedMobileNavigation]: !isNavigationMobileVisible
-                    })}>
-                      <div
-                        className={styles.navigationLinks__link}
-                        onClick={() => {
-                            if(isMobile) {
-                              scrollToAboutUs(950);
-                              closeMobileNavigation();
-                            } else {
-                                scrollToAboutUs(993);
-                            }
-                        }}
-                      >
-                        О нас
-                      </div>
-                      <div
-                        className={styles.navigationLinks__link}
-                        onClick={() => {
-                            if(isMobile) {
-                                scrollToAdvantages(2604);
-                                closeMobileNavigation();
-                            } else {
-                                scrollToAdvantages(2604);
-                            }
-                        }}
-                      >
-                        Преимущества
-                      </div>
-                      <div
-                        className={styles.navigationLinks__link}
-                        onClick={() => {
-                          if(isMobile) {
-                              scrollToThemes(3770);
-                              closeMobileNavigation();
-                          } else {
-                              scrollToThemes(3700);
-                          }
-                        }}
-                      >
-                        Категории и темы
-                      </div>
-                      <div
-                        className={styles.navigationLinks__link}
-                        onClick={() => {
-                            if(isMobile) {
-                                scrollToContacts(4777);
-                                closeMobileNavigation();
-                            } else {
-                              scrollToContacts(4737);
-                            }
-                        }}
-                      >
-                        Контакты
-                      </div>
-                      {Cookies.get('token') 
-                        ? 
-                            <MyButton 
-                                size="small"
-                                color="primary" 
-                                variant="outlined" 
-                                btnClassName={styles.entranceBtnMobile} 
-                                onClick={() => navigate('/cabinet/appeals')}
-                            >
-                                Профиль
-                            </MyButton>
-                        : 
-                          <MyButton 
-                              size="small"
-                              color="primary" 
-                              variant="outlined" 
-                              btnClassName={styles.entranceBtnMobile} 
-                              onClick={() => {
-                                closeMobileNavigation();
-                                openSignInModal();
-                              }}
-                          >
-                            Войти
-                          </MyButton>
-                        }
+            <div className={styles.navigationInner}>
+
+                <Link to='/'>
+                  <div className={styles.navigationInner__logo}>
+                      <img src={logo} alt="logo" />
                   </div>
-
-
-                <div onClick={isNavigationMobileVisible ? closeMobileNavigation : openMobileMavigation} className={classNames(styles.burgerMenu, {
-                  [styles.openedNavigationBurgerMenu]: isNavigationMobileVisible,
+                </Link>
+                <div className={classNames(styles.navigationLinks, {
+                  [styles.activeMobileNavigation]: isNavigationMobileVisible,
+                  [styles.closedMobileNavigation]: !isNavigationMobileVisible
                 })}>
-                  <div className={classNames(styles.burgerMenu__line, styles.line1)}></div>
-                  <div className={classNames(styles.burgerMenu__line, styles.line2)}></div>
-                  <div className={classNames(styles.burgerMenu__line, styles.line3)}></div>
+                  <div
+                    className={styles.navigationLinks__link}
+                    onClick={() => {
+                        if(isMobile) {
+                          scrollToAboutUs(950);
+                          closeMobileNavigation();
+                        } else {
+                            scrollToAboutUs(993);
+                        }
+                    }}
+                  >
+                    О нас
+                  </div>
+                  <div
+                    className={styles.navigationLinks__link}
+                    onClick={() => {
+                        if(isMobile) {
+                            scrollToAdvantages(2604);
+                            closeMobileNavigation();
+                        } else {
+                            scrollToAdvantages(2604);
+                        }
+                    }}
+                  >
+                    Преимущества
+                  </div>
+                  <div
+                    className={styles.navigationLinks__link}
+                    onClick={() => {
+                      if(isMobile) {
+                          scrollToThemes(3770);
+                          closeMobileNavigation();
+                      } else {
+                          scrollToThemes(3700);
+                      }
+                    }}
+                  >
+                    Категории и темы
+                  </div>
+                  <div
+                    className={styles.navigationLinks__link}
+                    onClick={() => {
+                        if(isMobile) {
+                            scrollToContacts(4777);
+                            closeMobileNavigation();
+                        } else {
+                          scrollToContacts(4737);
+                        }
+                    }}
+                  >
+                    Контакты
+                  </div>
                 </div>
 
                 <div className={styles.navigationEntrance}>
@@ -182,7 +149,6 @@ const Navigation: React.FC = React.memo(function Navigation() {
                                 Профиль
                             </MyButton>
                             <ProfilePopup closeProfilePopup={closeProfilePopup} isPopupVisible={isProfilePopupVisible} />
-
                         </div>
                     :
                         <MyButton 
@@ -197,6 +163,16 @@ const Navigation: React.FC = React.memo(function Navigation() {
                     }
                 </div>
 
+                <div 
+                    onClick={isNavigationMobileVisible ? closeMobileNavigation : openMobileMavigation} 
+                    className={classNames(styles.burgerMenu, {
+                        [styles.openedNavigationBurgerMenu]: isNavigationMobileVisible,
+                    })}
+                >
+                    <div className={classNames(styles.burgerMenu__line, styles.line1)}></div>
+                    <div className={classNames(styles.burgerMenu__line, styles.line2)}></div>
+                    <div className={classNames(styles.burgerMenu__line, styles.line3)}></div>
+                </div>
               </div>
             </div>
         </header>
