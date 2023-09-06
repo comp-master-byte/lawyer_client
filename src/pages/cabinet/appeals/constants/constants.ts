8547c8962d78e672a37dfde8a9d3600febba39dd
comp-master-byte <demoniac781@gmail.com>
2023-09-06 19:13:27 +0300

    Статуст candidates

--- src/pages/cabinet/appeals/constants/constants.ts
+++ src/pages/cabinet/appeals/constants/constants.ts
@@ -2,7 +2,7 @@ import { ISelectOption } from "shared/model/types";
 
 export const APPEALS: ISelectOption[] = [
     {id: 1, value: 'Все обращения', label: ''},
-    {id: 2, value: 'Ожидает выбора юриста', label: 'new'},
+    {id: 2, value: 'Ожидает выбора юриста', label: 'candidates'},
     {id: 3, value: 'Идет диалог с юристом', label: 'active'},
     {id: 4, value: 'Черновик', label: ''},
     {id: 5, value: 'Завершено', label: 'complete'},
