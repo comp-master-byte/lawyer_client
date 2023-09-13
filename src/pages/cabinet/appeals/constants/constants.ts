d51b90d2fd8c062d1717fae6899aacbbeebaf519
comp-master-byte <demoniac781@gmail.com>
2023-09-13 22:04:12 +0300

    Фильтр заявок

--- src/pages/cabinet/appeals/constants/constants.ts
+++ src/pages/cabinet/appeals/constants/constants.ts
@@ -3,8 +3,8 @@ import { ISelectOption } from "shared/model/types";
 export const APPEALS: ISelectOption[] = [
     {id: 1, value: 'Все обращения', label: ''},
     {id: 2, value: 'Ожидает выбора юриста', label: 'candidates'},
-    {id: 3, value: 'Идет диалог с юристом', label: 'active'},
-    {id: 4, value: 'Черновик', label: 'draft'},
-    {id: 5, value: 'Завершено', label: 'complete'},
-    {id: 6, value: 'Ожидает откликов от юристов', label: 'new'}
+    {id: 3, value: 'Ожидает откликов от юристов', label: 'new'},
+    {id: 4, value: 'Идет диалог с юристом', label: 'active'},
+    {id: 5, value: 'Черновик', label: 'draft'},
+    {id: 6, value: 'Завершено', label: 'complete'},
 ]
