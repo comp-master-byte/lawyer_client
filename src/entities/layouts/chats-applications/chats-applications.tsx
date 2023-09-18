dbb7655fb131e2f97e5368b6ad07051b8c0fea43
comp-master-byte <demoniac781@gmail.com>
2023-09-18 15:32:14 +0300

    Улучшение верстки

--- src/entities/layouts/chats-applications/chats-applications.tsx
+++ src/entities/layouts/chats-applications/chats-applications.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import styles from "./chats-applications.module.scss";
 import { Outlet, useParams } from 'react-router-dom';
 import { useAppDispatch, useTypedSelector } from 'shared/lib/hooks/redux';
@@ -9,7 +9,6 @@ import { fetchChatList } from './model/async-actions';
 import EmptyChat from './components/empty-chat/empty-chat';
 import { fetchMessages } from 'pages/cabinet/chat-with-lawyer/model/async-actions';
 import { clientChatSlice } from 'pages/cabinet/chat-with-lawyer/model/clientChatSlice';
-import { chatsApplicationsSlice } from './model/chatsApplicationsSlice';
 
 interface ApplicationsValues {
     search: string;
@@ -20,33 +19,14 @@ const ChatsApplications: React.FC = () => {
     const {register} = useForm<ApplicationsValues>();
     const dispatch = useAppDispatch();
 
-    const websocket = useRef<WebSocket>();
 
     const {resetMessages} = clientChatSlice.actions;
-    const {toggleWebsocketConnection} = chatsApplicationsSlice.actions;
 
-    const {chatList, isWebsocketConnected} = useTypedSelector(state => state.chatsApplicationsSlice);
+    const {chatList} = useTypedSelector(state => state.chatsApplicationsSlice);
 
-    const onSelectAndConnectChat = (chatId: number, userId: number) => {
+    const onSelectAndConnectChat = (chatId: number) => {
         dispatch(resetMessages());
-        dispatch(fetchMessages(chatId));
-
-        if(isWebsocketConnected) {
-            console.log("close");
-            
-            websocket.current!.onclose = function(event) {
-                // закрываем предыдущее соединение если оно есть
-                console.log("Обрываем соединение");
-                
-            }
-            dispatch(toggleWebsocketConnection(false));
-        }
-
-        websocket.current = new WebSocket(`wss://backend.juraprav.ru/ws/chat/${chatId}/${userId}`);
-        websocket.current.onopen = function() {
-            console.log('Соединение с чатом установлена', chatId);
-        }
-        dispatch(toggleWebsocketConnection(true));
+        dispatch(fetchMessages(chatId));        
     }
 
     useEffect(() => {
--- src/pages/cabinet/chat-with-lawyer/chat-with-lawyer.tsx
+++ src/pages/cabinet/chat-with-lawyer/chat-with-lawyer.tsx
@@ -1,10 +1,12 @@
-import React  from 'react';
+import React, { useEffect, useRef }  from 'react';
 import styles from "./chat-with-lawyer.module.scss";
 import MyInput from 'shared/ui/MyInput/MyInput';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import MyButton from 'shared/ui/MyButton/MyButton';
 import MessageList from './components/message-list/message-list';
-import { useTypedSelector } from 'shared/lib/hooks/redux';
+import { useAppDispatch, useTypedSelector } from 'shared/lib/hooks/redux';
+import { chatsApplicationsSlice } from 'entities/layouts/chats-applications/model/chatsApplicationsSlice';
+import { useParams } from 'react-router-dom';
 
 interface IChatValues {
     message: string
@@ -12,14 +14,41 @@ interface IChatValues {
 
 const ChatWithLawyer: React.FC = () => {
     const {register, reset, handleSubmit} = useForm<IChatValues>();
+    const websocket = useRef<WebSocket>();
+    const dispatch = useAppDispatch();
+    const {id} = useParams();
 
+    const {user} = useTypedSelector(state => state.userSlice);
     const {messages} = useTypedSelector(state => state.clientChatSlice);
+    const {toggleWebsocketConnection} = chatsApplicationsSlice.actions;
+    const {isWebsocketConnected} = useTypedSelector(state => state.chatsApplicationsSlice);
 
     const onSendMessage: SubmitHandler<IChatValues> = (data) => {
-        console.log(data);
-        
-        // reset({message: ""})
+        const toStringData = JSON.stringify(data);
+        websocket.current?.send(toStringData);
+        reset({message: ""});
     }    
+
+    useEffect(() => {
+        if(isWebsocketConnected) {
+            websocket.current!.onclose = function(event) {
+                // закрываем предыдущее соединение если оно есть
+                console.log("Обрываем соединение");
+            }
+            dispatch(toggleWebsocketConnection(false));
+        }
+    
+        if(id && user) {
+            console.log(id);
+            
+            websocket.current = new WebSocket(`wss://backend.juraprav.ru/ws/chat/${id}/${user.id}`);
+            websocket.current.onopen = function() {
+                console.log('Соединение с чатом установлена', id);
+            }
+            dispatch(toggleWebsocketConnection(true));
+        }
+    }, [id, user])
+
 
     return (
         <div className={styles.chatWithLawyerWrapper}>
