0e010a81d4223279e0afddd1951a17899ff0f8b1
comp-master-byte <demoniac781@gmail.com>
2023-09-26 01:11:28 +0300

    ФИО отправителя прикрутил

--- src/pages/cabinet/chat-with-lawyer/components/message-item/message-item.tsx
+++ src/pages/cabinet/chat-with-lawyer/components/message-item/message-item.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styles from "./message-item.module.scss";
 import classNames from 'classnames';
 import { useTypedSelector } from 'shared/lib/hooks/redux';
@@ -10,13 +10,18 @@ interface MessageItemProps {
 
 const MessageItem: React.FC<MessageItemProps> = ({message}) => {
     const {user} = useTypedSelector(state => state.userSlice);
+
+    const IS_LAWYER = useMemo(() => {
+        return user?.id === message.sender.id
+    }, [user]);
+
     return (
         <div className={classNames(styles.messageItemWrapper, {
-            [styles.receivedMessageItemWrapper]: user?.id !== message.sender.id,
-            [styles.sentMessageItemWrapper]: user?.id === message.sender.id,
+            [styles.receivedMessageItemWrapper]: !IS_LAWYER,
+            [styles.sentMessageItemWrapper]: IS_LAWYER,
         })}>
             <div className={styles.messageContent}>
-                <div className={styles.messageLabel}>Вы</div>
+                <div className={styles.messageLabel}>{IS_LAWYER ? "Вы" : message.sender.full_name}</div>
                 <div className={styles.messageWrapper}>
                     {message.text}
                 </div>
