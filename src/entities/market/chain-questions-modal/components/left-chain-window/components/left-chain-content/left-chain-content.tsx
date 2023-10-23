054007fd636fbd79cf7097912be36acb27d46da3
comp-master-byte <demoniac781@gmail.com>
2023-10-23 20:22:19 +0300

    Композиция компонентов

---
+++ src/entities/market/chain-questions-modal/components/left-chain-window/components/left-chain-content/left-chain-content.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from "./left-chain-content.module.scss";
+import { QuestionFromChain } from 'shared/model/types';
+import BotStaticContent from 'shared/components/bot-static-content/bot-static-content';
+
+interface LeftChainContentProps {
+    chainData: QuestionFromChain|null;
+    isChainLoading: boolean;
+}
+
+const LeftChainContent: React.FC<LeftChainContentProps> = ({chainData, isChainLoading}) => {
+    if(isChainLoading||chainData?.node_id === 1) {
+        return <BotStaticContent />
+    }
+
+    return <p className={styles.contentHelperText}>{chainData?.message}</p>    
+}
+
+export default LeftChainContent;
--- src/entities/market/chain-questions-modal/components/left-chain-window/left-chain-window.tsx
+++ src/entities/market/chain-questions-modal/components/left-chain-window/left-chain-window.tsx
@@ -3,8 +3,8 @@ import styles from "./left-chain-window.module.scss";
 import BlueBorderModal from 'shared/ui/blue-border-modal/blue-border-modal';
 import LeftHeader from './components/left-header/left-header';
 import logoSvg from "shared/assets/logo.svg";
-import BotStaticContent from 'shared/components/bot-static-content/bot-static-content';
 import { QuestionFromChain } from 'shared/model/types';
+import LeftChainContent from './components/left-chain-content/left-chain-content';
 
 interface LeftChainWindowProps {
     chainData: QuestionFromChain|null;
@@ -19,10 +19,10 @@ const LeftChainWindow: React.FC<LeftChainWindowProps> = ({chainData, isChainLoad
                 <div className={styles.leftWindowLogo}>
                     <img src={logoSvg} alt='' />
                 </div>
-                {isChainLoading 
-                    ? <BotStaticContent /> : chainData?.node_id === 1 
-                    ? <BotStaticContent /> : <p className={styles.contentHelperText}>{chainData?.message}</p>
-                }
+                <LeftChainContent 
+                    chainData={chainData}
+                    isChainLoading={isChainLoading}
+                />
             </div>
         </BlueBorderModal>
     )
